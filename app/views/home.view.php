fc1f480f6c9a06da9b377073c1d2d112f5f3fad3
Roman Kozlov <rkozlov95@mail.ru>
2020-05-03 18:22:35 +0300

    add basics templates

--- app/views/home.view.php
+++ app/views/home.view.php
@@ -1,10 +1,22 @@
-<!doctype html>
-<html lang="en">
-  <head>
-      <meta charset="utf-8">
-      <title>Home</title>
-  </head>
-  <body>
-	Hello world!
-  </body>
-</html>
+<?php include __DIR__ . '/partials/header.php'; ?>
+
+<div class="card">
+    <div class="card-header">
+        Login
+    </div>
+    <div class="card-body">
+        <div class="form-group">
+            <label>Email</label>
+            <input class="form-control" required>
+        </div>
+        <div class="form-group">
+            <label>Password</label>
+            <input class="form-control" required>
+        </div>
+        <button type="submit" class="btn btn-primary">
+            Sign in
+        </button>
+    </div>
+</div>
+
+<?php include __DIR__ . '/partials/footer.php'; ?>
---
+++ app/views/partials/header.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="en">
+  <head>
+      <meta charset="utf-8">
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+      <link rel="stylesheet" type="text/css" href="public/css/custom.css">
+  </head>
+  <body>
+      <div class="container h-100">
+          <div class="row h-100 justify-content-center align-items-center">
+              <div class="col-md-5">
