9d1681191348cdae2588e4bf2ab712bb8bf558ff
Roman Kozlov <rkozlov95@mail.ru>
2020-05-12 22:40:36 +0300

    update/extended database class

--- app/core/Database.php
+++ app/core/Database.php
@@ -37,6 +37,27 @@ class Database
         $this->statement = $this->pdo->prepare($query);
     }
 
+    public function bind($param, $value, $type = null)
+    {
+        if (is_null($type)) {
+            switch (true) {
+                case is_int($value):
+                    $type = PDO::PARAM_INT;
+                    break;
+                case is_bool($value):
+                    $type = PDO::PARAM_BOOL;
+                    break;
+                case is_null($value):
+                    $type = PDO::PARAM_NULL;
+                    break;
+                default:
+                    $type = PDO::PARAM_STR;
+            }
+        }
+
+        $this->statement->bindValue($param, $value, $type);
+    }
+
     public function executeStatement()
     {
         try {
@@ -46,12 +67,17 @@ class Database
         }
     }
 
+    public function result()
+    {
+        $this->executeStatement();
+
+        return $this->statement->fetch(PDO::FETCH_ASSOC);
+    }
+
     public function selectAll($table)
     {
         $this->prepareQuery("select * from {$table}");
 
-        $this->executeStatement();
-
-        return $this->statement->fetchAll(PDO::FETCH_ASSOC);
+        return $this->result();
     }
 }
