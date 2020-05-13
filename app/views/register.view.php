810b9c42ab5f635c52c2ddfb6fbaaee3b090b3a2
Roman Kozlov <rkozlov95@mail.ru>
2020-05-13 19:30:02 +0300

    fix register template

--- app/views/register.view.php
+++ app/views/register.view.php
@@ -1,54 +1,56 @@
 <?php include __DIR__ . '/partials/header.php'; ?>
 
-<div class="card">
+<div class="card shadow p-1">
     <div class="card-header">
         <?php echo $lang['Registration'] ?>
     </div>
     <div class="card-body">
-        <div class="form-group">
-            <label>
-                <?php echo $lang['Username'] ?>
-            </label>
-            <input class="form-control" required>
-        </div>
-        <div class="form-group">
-            <label>
-                <?php echo $lang['Email'] ?>
-            </label>
-            <input class="form-control" required>
-        </div>
-        <div class="form-group">
-            <label>
-                <?php echo $lang['Password'] ?>
-            </label>
-            <input class="form-control" required>
-        </div>
-        <div class="form-group">
-            <label>
-                <?php echo $lang['Repeat_password'] ?>
-            </label>
-            <input class="form-control" required>
-        </div>
-        <div class="form-group">
-            <label>
-                <?php echo $lang['About'] ?>
-            </label>
-            <textarea class="form-control" rows="4"></textarea>
-        </div>
-        <div class="form-group">
-            <label>
-                <?php echo $lang['Add_photo'] ?>
-            </label>
-            <input type="file" class="form-control-file">
-        </div>
-        <div class="form-group d-flex align-items-center">
-            <button type="submit" class="btn btn-primary mr-auto">
-                <?php echo $lang['Submit'] ?>
-            </button>
-            <a href="/">
-                <?php echo $lang['Already_have_account'] ?>
-            </a>
-        </div>
+        <form action="/register" method="post" enctype="multipart/form-data">
+            <div class="form-group">
+                <label>
+                    <?php echo $lang['Username'] ?>
+                </label>
+                <input class="form-control" name="username" required>
+            </div>
+            <div class="form-group">
+                <label>
+                    <?php echo $lang['Email'] ?>
+                </label>
+                <input class="form-control" name="email" required>
+            </div>
+            <div class="form-group">
+                <label>
+                    <?php echo $lang['Password'] ?>
+                </label>
+                <input class="form-control" name="password" required>
+            </div>
+            <div class="form-group">
+                <label>
+                    <?php echo $lang['Repeat_password'] ?>
+                </label>
+                <input class="form-control" name="repeat_password" required>
+            </div>
+            <div class="form-group">
+                <label>
+                    <?php echo $lang['About'] ?>
+                </label>
+                <textarea class="form-control" rows="4" name="about"></textarea>
+            </div>
+            <div class="form-group">
+                <label>
+                    <?php echo $lang['Add_photo'] ?>
+                </label>
+                <input type="file" class="form-control-file" name="image" required>
+            </div>
+            <div class="form-group d-flex align-items-center">
+                <button type="submit" class="btn btn-primary mr-auto">
+                    <?php echo $lang['Submit'] ?>
+                </button>
+                <a href="/">
+                    <?php echo $lang['Already_have_account'] ?>
+                </a>
+            </div>
+        </form>
     </div>
 </div>
 
