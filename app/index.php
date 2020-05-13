36568db0086378f2c261e319a37cb3d581a1438b
Roman Kozlov <rkozlov95@mail.ru>
2020-05-13 21:10:50 +0300

    fix index

--- app/index.php
+++ app/index.php
@@ -4,7 +4,7 @@ use core\Router;
 use core\Request;
 
 spl_autoload_register(function ($class) {
-    $path = str_replace('\\', '/', $class.'.php');
+    $path = str_replace('\\', '/', $class . '.php');
     if (file_exists($path)) {
         require $path;
     }
