8033ec12ef2a94701e639d051b94f2fa54ea000d
Roman Kozlov <rkozlov95@mail.ru>
2020-05-15 17:11:12 +0300

    updated translations

--- app/languages/en.php
+++ app/languages/en.php
@@ -1,11 +1,12 @@
 <?php
     $lang = [
+        /* template internalization */
         "Login" => "Login",
         "Registration" => "Registration",
         "Username" => "Username",
         "Email" => "Email",
         "Password" => "Password",
-        "Repeat_password" => "Repeat password",
+        "Confirm_password" => "Confirm password",
         "Sign_in" => "Sign in",
         "Sign_up" => "Sign up",
         "Dont_have_account" => "Don't have account?",
@@ -13,6 +14,24 @@
         "Add_photo" => "Add photo",
         "Submit" => "Submit",
         "About" => "About you",
+        "Logout" => "Logout",
         "lang_en" => "English",
         "lang_ru" => "Russian",
+
+        /* authentification errors */
+        "Email_not_exists" => "Email address not found in our system!",
+        "Login_fail" => "Incorrect email / password combination!",
+
+        /* registration errors */
+        "Username_too_short" => "Username must contain at least 3 characters!",
+        "Username_too_long" => "Username must be shorter than 50 characters!",
+        "Username_exists" => "That username already exists!",
+        "Username_contains_disallowed" => "Username cannot contain special characters!",
+        "Password_missmatch" => "Password missmatch!",
+        "Password_too_short" => "Password must contain at least 8 characters!",
+        "Email_exists" => "Email address already exists!",
+        "Incorrect_email" => "Incorrect email adress!",
+        "About_too_short" => "The field about you cannot contain less than 3 characters!",
+        "File_size_max" => "File exceeds maximum size 5 MB!",
+        "Not_allowed_file_extension" => "This file extension is not allowed. Please upload a JPEG or PNG file!",
     ];
